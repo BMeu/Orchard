ad4effbdf95b51f151d613f02f70b4501bbe453d
BMeu <bastian@bastianmeyer.eu>
2016-10-14 09:13:48 +0200

    Set default locale in test to avoid test failures when different default is used than expected.

--- tests/unit/extensions/flask_babel_unit_test.py
+++ tests/unit/extensions/flask_babel_unit_test.py
@@ -14,6 +14,7 @@ class BabelUnitTest(unittest.TestCase):
 
     def setUp(self):
         self.app = orchard.create_app('Testing')
+        self.app.config['BABEL_DEFAULT_LOCALE'] = 'en'
         self.app.config['LANGUAGES'] = {
             'de': 'Deutsch',
             'en': 'English'
