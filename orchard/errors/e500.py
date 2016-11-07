31ec9a0ae45c42c79f0e2edba3f11fc0578f33c4
BMeu <bastian@bastianmeyer.eu>
2016-11-07 22:46:59 +0100

    Send mail to admins on all internal server errors.

--- orchard/errors/e500.py
+++ orchard/errors/e500.py
@@ -4,6 +4,8 @@
     This module sets up the view for handling ``500 Internal Server Error`` errors.
 """
 
+import datetime
+
 import flask
 import flask_classful
 
@@ -20,10 +22,24 @@ class Error500View(flask_classful.FlaskView):
     @blueprint.app_errorhandler(Exception)
     def index(self) -> str:
         """
-            Display the error page.
+            Display the error page for internal errors and send a mail to all administrators
+            information them of this error.
 
             :return: A page explaining the error.
         """
+        message = ('Time:      {time}\n' +
+                   'Request:   {method} {path}\n' +
+                   'Agent:     {agent_platform} | {agent_browser} {agent_browser_version}\n' +
+                   'Raw Agent: {agent}\n\n'
+                   ).format(time = datetime.datetime.now(),
+                            method = flask.request.method,
+                            path = flask.request.path,
+                            agent_platform = flask.request.user_agent.platform,
+                            agent_browser = flask.request.user_agent.browser,
+                            agent_browser_version = flask.request.user_agent.version,
+                            agent = flask.request.user_agent.string)
+
+        flask.current_app.logger.exception(message)
         return flask.render_template('errors/500.html')
 
 Error500View.register(blueprint)
