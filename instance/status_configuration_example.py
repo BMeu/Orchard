8db869beb1c953782219f0bb4d70c48cb4983850
BMeu <bastian@bastianmeyer.eu>
2016-11-11 09:27:17 +0100

    Display actual system information.

--- instance/status_configuration_example.py
+++ instance/status_configuration_example.py
@@ -7,23 +7,77 @@
 import flask_babel
 
 from orchard.system_status import StatusGroup, StatusItem
+from orchard.system_status.system import cpu, memory, network, os, storage
 
-group_system = StatusGroup(flask_babel.gettext('System'))
-status_kernel = StatusItem(flask_babel.gettext('Kernel'), str,
-                           ['Linux raspberry 4.4.30-v7+ #919, November 1st, 2016, 16:57:28h'])
-status_gpu = StatusItem(flask_babel.gettext('GPU'), str,
-                        ['f74adfcae00b721627eab44590728c13860bcbc2, October 25th, 2016, 16:03:42h'])
-status_runtime = StatusItem(flask_babel.gettext('Runtime'), str, ['3 days, 02:28:39 hours'])
-group_system.append(status_kernel)
-group_system.append(status_gpu)
-group_system.append(status_runtime)
-
+# Create the status groups.
+group_cpu = StatusGroup(flask_babel.gettext('CPU'))
+group_memory = StatusGroup(flask_babel.gettext('Memory'))
+group_network = StatusGroup(flask_babel.gettext('Network'))
+group_os = StatusGroup(flask_babel.gettext('Operating System'))
+group_storage = StatusGroup(flask_babel.gettext('Storage'))
 group_users = StatusGroup(flask_babel.gettext('Users'))
-status_logins = StatusItem(flask_babel.gettext('SSH Logins'), str,
-                           ['Two users are currently logged in.'])
-status_last_login = StatusItem(flask_babel.gettext('Last Login'), str,
-                               ['November 8th, 2016, 00:07:18h, on fe80::108e:d4a8:'])
-group_users.append(status_logins)
+
+# Operating System.
+status_kernel = StatusItem(flask_babel.gettext('Kernel'), os.kernel_version)
+status_gpu = StatusItem(flask_babel.gettext('GPU'), os.gpu_version)
+status_runtime = StatusItem(flask_babel.gettext('Runtime'), os.run_time)
+status_current_time = StatusItem(flask_babel.gettext('Current Time'), os.current_time)
+group_os.append(status_kernel)
+group_os.append(status_gpu)
+group_os.append(status_runtime)
+group_os.append(status_current_time)
+
+# Users.
+status_current_logins = StatusItem(flask_babel.gettext('Current Logins'), os.current_logins)
+status_last_login = StatusItem(flask_babel.gettext('Last Login'), os.last_login)
+group_users.append(status_current_logins)
 group_users.append(status_last_login)
 
-Statuses = [group_system, group_users]
+# CPU.
+status_temperature = StatusItem(flask_babel.gettext('Temperature'), cpu.temperatue)
+status_processes = StatusItem(flask_babel.gettext('Running Processes'), os.running_processes)
+status_load_one = StatusItem(flask_babel.gettext('Load (1-minute average)'), cpu.load,
+                             [cpu.LoadPeriods.one])
+status_load_five = StatusItem(flask_babel.gettext('Load (5-minute average)'), cpu.load,
+                              [cpu.LoadPeriods.five])
+status_load_fifteen = StatusItem(flask_babel.gettext('Load (15-minute average)'), cpu.load,
+                                 [cpu.LoadPeriods.fifteen])
+group_cpu.append(status_temperature)
+group_cpu.append(status_processes)
+group_cpu.append(status_load_one)
+group_cpu.append(status_load_five)
+group_cpu.append(status_load_fifteen)
+
+# Storage.
+status_storage_total = StatusItem(flask_babel.gettext('Total'), storage.total)
+status_storage_used = StatusItem(flask_babel.gettext('Used'), storage.used)
+status_storage_available = StatusItem(flask_babel.gettext('Available'), storage.available)
+group_storage.append(status_storage_total)
+group_storage.append(status_storage_used)
+group_storage.append(status_storage_available)
+
+# Memory.
+status_mem_total = StatusItem(flask_babel.gettext('Total'), memory.total)
+status_mem_used = StatusItem(flask_babel.gettext('Used'), memory.used)
+status_mem_available = StatusItem(flask_babel.gettext('Available'), memory.available)
+status_swap_total = StatusItem(flask_babel.gettext('Swap: Total'), memory.swap_total)
+status_swap_used = StatusItem(flask_babel.gettext('Swap: Used'), memory.swap_used)
+status_swap_available = StatusItem(flask_babel.gettext('Swap: Available'), memory.swap_available)
+group_memory.append(status_mem_total)
+group_memory.append(status_mem_used)
+group_memory.append(status_mem_available)
+group_memory.append(status_swap_total)
+group_memory.append(status_swap_used)
+group_memory.append(status_swap_available)
+
+# Network.
+status_ip_external = StatusItem(flask_babel.gettext('External IP'), network.external_ip_address)
+status_ip_lan0 = StatusItem(flask_babel.gettext('Internal IP (Ethernet)'), network.ip_address,
+                            ['lan0', network.IPVersion.v4])
+status_ip_wlan0 = StatusItem(flask_babel.gettext('Internal IP (WLAN)'), network.ip_address,
+                             ['wlan0', network.IPVersion.v4])
+group_network.append(status_ip_external)
+group_network.append(status_ip_lan0)
+group_network.append(status_ip_wlan0)
+
+Statuses = [group_os, group_users, group_cpu, group_storage, group_memory, group_network]
